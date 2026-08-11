fce448f9bdfb7f712972c11ada5ac07d44a6503e
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-11 21:07:23 +0530

    Search element in Vector - Time: O(log n), Space: O(1)

---
+++ 701624-search-element-in-vector-GFG/701624-search-element-in-vector.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    bool binarySearch(vector<int>& arr, int k) {
+        // code here
+        return binary_search(arr.begin(), arr.end(), k);
+    }
+};
+
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
