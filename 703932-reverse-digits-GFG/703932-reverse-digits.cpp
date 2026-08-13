ddedabf695de1a145f44338f7bf52fe4485f0ca3
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-13 23:54:15 +0530

    reverse number - Time: O(log n), Space: O(1)

---
+++ 703932-reverse-digits-GFG/703932-reverse-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int reverseDigits(int n) {
+        // Code here
+        int lastd;
+        int rev=0;
+        while(n>0){
+            lastd=n%10;
+            rev=rev*10+lastd;
+            n/=10;
+        }
+        return rev;
+        
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
