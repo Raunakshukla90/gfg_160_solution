5c27c7ddf55bb4d5bc4f68a020ee5d1547bde488
Raunak Shukla <raunakshukla5861@gmail.com>
2026-08-27 22:06:06 +0530

    armstrong-numbers2

---
+++ 703783--GFG/703783-.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool armstrongNumber(int n) {
+        int original = n;
+        int sum = 0;
+
+        while(n > 0) {
+            int digit = n % 10;
+            sum += digit * digit * digit;
+            n /= 10;
+        }
+
+        return sum == original;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
