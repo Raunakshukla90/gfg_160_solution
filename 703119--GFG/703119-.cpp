b29d98b480bbf92b7c47071cf19ef0aee9cd7d28
Raunak Shukla <raunakshukla5861@gmail.com>
2026-09-23 22:09:52 +0530

    Search Pattern

---
+++ 703119--GFG/703119-.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    vector<int> search(string& pat, string& txt) {
+        vector<int>ans;
+        size_t pos=txt.find(pat);
+        while(pos!=string::npos){
+            ans.push_back(pos);
+            pos=txt.find(pat,pos+1);
+        }
+        return ans;
+    }
+};
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
